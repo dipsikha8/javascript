e4eaef7ea18c279f1eda300349a8ed363d07361a
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-03-27 11:27:24 +0000

    varibale and datatype lessions

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict";
+//alert(3+3)//we are using nodejs
+console.log(3+3) //code readability should be high
+console.log("dipu")
+let name="Dipsikha"
+let age =18
+let isloggedIn=false
+let state;
+//premitive datatypes
+//number =>2 to power 53 
+//bigint
+//string =>""
+//boolean=true or false
+//null=>standalone value
+//undefined=>did not assign value yet
+//symbol=>unique
+
+//objects
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
---
+++ 01_Basics/variable.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="dispikha20018@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+//accountId=2
+
+
+console.table([accountId,accountEmail,accountpassword]);
