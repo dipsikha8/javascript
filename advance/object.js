201cc8e803c8c906a0570b5332c21f6136ec25d0
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-04-03 06:15:43 +0000

    object

---
+++ advance/object.js
@@ -0,0 +1,15 @@
+//singleton-> Object.create
+
+
+//object literals
+
+const jsuser={
+    name:"Dipsikha",
+    age:22,
+    location:"burdwan",
+    isloggedin:false,
+    email:"dipsikha200218@gmail.com"
+
+
+}
+console.log
