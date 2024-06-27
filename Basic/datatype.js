a0fdd40bfb31d59c1a0eb7a6a4f4abd98c367d7d
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-06-27 14:00:41 +0000

    save

---
+++ Basic/datatype.js
@@ -0,0 +1,24 @@
+"use strict";
+//alert(3+3)//we are using nodejs . we are not using browser
+console.log(3+3) //code readability should be high
+console.log("dipu")
+let name="Dipsikha"//string
+let age =18//number
+let isloggedIn=false//boolean
+let state;
+//premitive datatypes
+//number =>2 to power 53 
+//bigint
+//string =>""
+//boolean=true or false
+//null=>standalone value.it is considered as empty value.
+//undefined=>did not assign value yet
+//symbol=>unique
+
+//objects
+
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+//null is a object .//but undefined is a type
+
