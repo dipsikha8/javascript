94d88e7d0837462b5f4bec2e9183efead2515b90
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-06-27 13:55:16 +0000

    conversion of datatypes

--- 01_Basics/typeconversion.js
+++ 01_Basics/typeconversion.js
@@ -12,4 +12,27 @@ console.log(typeof valueInNumber1);//number
 console.log(valueInNumber1);//0
 
 
+let islogged=1
+let booleanIslogged=Boolean(islogged)
+console.log(booleanIslogged);
+
+
+let some=34
+let string=String(some)
+console.log(some)
+console.log(typeof string)
+
+
+//basic conversion
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+//1=>true;0=>false
+//""=>false
+//"dipu"=>true
+
+
+
+
 
