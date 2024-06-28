398627fdd06ebcb70d23a487c813e43f25575e62
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-06-28 02:48:47 +0000

    conversions

--- Basic/cm.js
+++ Basic/cm.js
@@ -9,7 +9,13 @@ console.log("02">1);
 
 console.log(null>0)//false
 console.log(null==0)//false
-console.log(null>=0)//true//conversion problem
+console.log(null>=0)//true//conversion problem//we always try to avoid this type of conventions
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+
 
 //The reason is that an equality check== and comparison><>=<=work differently.
 
