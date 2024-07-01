773d807bcfed63bc431b56506d9ce8a334da3ca1
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-07-01 12:13:53 +0000

    number and mathsr

---
+++ Basic/numberandmaths.js
@@ -0,0 +1,15 @@
+const score=400
+
+
+const balance=new Number(100)
+console.log(balance)//it specifically defined it as number
+console.log(score)//it just prints the value
+
+
+console.log(balance.toString());//it converts it to string
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//it gives precision value
+const otherNumber=123.8966
+console.log(otherNumber.toPrecision(3));
+const hundreds=1000000
+console.log(hundreds.toLocaleString());//it converts it to us standards.
