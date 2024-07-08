dfe6897939e3e06464b13b09d937afe09fe55268
dipsikha8 <144507817+dipsikha8@users.noreply.github.com>
2024-07-08 12:22:41 +0000

    objects

--- advance/object.js
+++ advance/object.js
@@ -34,8 +34,8 @@ jsuser.greeting2=function(){
 
 }
 console.log(jsuser.greeting2())
-const tinder=new object()//singletone object
-const tinderuser={}//non singletone object
+//const tinder=new object()//singletone object//if u print it it is empty object
+//const tinderuser={}//non singletone object
 
 
 const tin={}
@@ -46,7 +46,7 @@ console.log(tin);
 
 const regular={
     email:"some@gmail.com",
-    fullname:{
+    fullname:{                     //you can go with nesting
         userfullname:{
           firstname:"Dipsikha",
           lastname:"Mazumdar"
@@ -54,16 +54,21 @@ const regular={
 
     }
 }
-console.log(regular.fullname);
+console.log(regular.fullname.userfullname.firstname);//we can access value using '.' 
+
 
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
-const obj3={obj1,obj2}
-console.log(obj3);
+const obj3={obj1,obj2}//same like array problem object will come into an another object
+//console.log(obj3);
 
-constobj4=object.assign({},obj1,obj2)
+const obj4=Object.assign(obj1,obj2)//u can use this syntax
 console.log(obj4);
-const users = [
+const obj5=Object.assign({},obj1,obj2)//this will also give same result but it is mostly preferrable .//sometimes there can be more object therefore we use {}.{}is the target and all the objects are source.
+console.log(obj5);//it returns a modified target object
+const obj6={...obj1, ...obj2}//u can use spread operator also like array .This is the latest 
+console.log(obj6);
+const users = [//array of objects//u can add multiple objects 
     {
         id: 1,
         email: "h@gmail.com"
@@ -78,13 +83,13 @@ const users = [
     },
 ]
 
-users[1].email
+users[1].email//as this is the array so give the index then use .as u want to access object
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(Object.keys(tin));//u are making array of using the keys of tinderuser object
+// console.log(Object.values(tin));
+// console.log(Object.entries(tin));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));//if it is present or not 
 
 
 const course = {
@@ -103,14 +108,14 @@ const {courseInstructor: instructor} = course//destrcutute of object
 console.log(instructor);
 
 // {
-//     "name": "hitesh",
+//     "name": "hitesh",//keys are also a string in json
 //     "coursename": "js in hindi",//json{key:value both are string}
 //     "price": "free"
 // }
 
 [
     {},
-    {},            
+    {},            //API in array format
     {}
 ]
 
